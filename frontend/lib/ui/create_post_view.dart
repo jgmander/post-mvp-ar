722f53a89ef923b51943b428b271e539467285b9
Jason Mander <jgmander@gmail.com>
2026-03-05 22:21:20 -0500

    Add runtime location permission request to CreatePostView

--- frontend/lib/ui/create_post_view.dart
+++ frontend/lib/ui/create_post_view.dart
@@ -19,6 +19,18 @@ class _CreatePostViewState extends State<CreatePostView> {
     setState(() => _isLoading = true);
 
     try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          throw Exception("Location permissions are denied");
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        throw Exception("Location permissions are permanently denied, we cannot request permissions.");
+      }
+
       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
       
       final newPost = Post(
